eed7788ed870758158b9cdd646bba9f69fad0309
jezhische <jezhische@gmail.com>
2016-07-05 08:35:01 +0300

    PrintfTest.java adding

---
+++ src/main/java/PrintfTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Ежище on 05.07.2016.
+ */
+public class PrintfTest {
+    static String name = "War and Peace";
+    static String author = "Толстой";
+    static int year = 1870;
+
+    static public void Info () {
+        System.out.printf("Книга '%s' (автор %s) была издана в %d году \n", name, author, year);
+    }
+    public static void main(String[] args) {
+        PrintfTest.Info();
+    }
+
+}
