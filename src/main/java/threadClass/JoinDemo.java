465e554802efd202f47585c166b7289fa4c37be1
jezhische <jezhische@gmail.com>
2016-09-08 18:45:40 +0300

    learning concurrent threads...

---
+++ src/main/java/threadClass/JoinDemo.java
@@ -0,0 +1,91 @@
+package threadClass;
+
+/**
+ * Created by WORK on 08.09.2016.
+ * http://darkraha.com/rus/java/api/multithread/join.php
+ */
+public class JoinDemo {
+    // задача для потоков: просто засыпаем на 4 мс count раз
+    public static class MyRunnable implements Runnable {
+        private int count;
+
+        public MyRunnable(int count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < count; ++i)
+                    Thread.sleep(4);
+            } catch (Exception e) {
+
+            }
+
+            System.out.println("thread " + Thread.currentThread().getName()
+                    + " finished");
+        }
+    }
+
+
+    //--------------------------------------------------------------
+    // correct - то что обычно мы ожидаем
+    //--------------------------------------------------------------
+    // в этом тесте вначале запускаем потоки,
+    // а потом в отдельном цикле подсоединяемся к запущенным потокам.
+    // В результате ждем завершения всех запущенных потоков,
+    // для проверки специально в середину засунул самый длинный поток
+    public void test2() {
+        int count;
+        Thread pool[] = new Thread[10];
+
+        try {
+            for (int i = 9; i >= 0; --i) {
+                count = 100 + 100 * i;
+                count = (i == 5) ? count + 800 : count;
+                pool[i] = new Thread(new MyRunnable(count), "test2_task" + i
+                        + "_" + count);
+                pool[i].start();
+            }
+
+            // for (int i = 0; i <10 ; ++i) {
+            for (int i = 9; i >= 0; --i) {
+                pool[i].join();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("test2 finished");
+    }
+
+    //--------------------------------------------------------------
+    // "incorrect" - вряд ли вы хотели это
+    //--------------------------------------------------------------
+    // в этом тесте запускается поток и сразу присоединяемся к нему
+    // в результате ждем завершение одного запущенного потока, и только потом запускаем следующий
+    public void test1() {
+        int count;
+        Thread t;
+
+        try {
+            for (int i = 9; i >= 0; --i) {
+                count = 100 + 100 * i;
+                t = new Thread(new MyRunnable(count), "test1_task" + count);
+                t.start();
+                t.join();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("test1 finished");
+    }
+
+    public static void main(String[] args) {
+        JoinDemo jd =new JoinDemo();
+        jd.test2();
+        jd.test1();
+    }
+}
