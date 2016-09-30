37fd0cb5623effc3478dc81d9373cf4313a67846
jezhische <jezhische@gmail.com>
2016-09-30 18:47:00 +0300

    adding some swing "window" classes

---
+++ src/main/java/swing/Win11JSlider.java
@@ -0,0 +1,27 @@
+package swing;
+
+import javax.swing.*;
+
+/**
+ * Created by WORK on 30.09.2016.
+ * http://math.sgu.ru/sites/chairs/prinf/materials/java/lesson9.htm
+ */
+public class Win11JSlider extends JFrame {
+    Win11JSlider() {
+        super("Пример использования JSlider");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 50, 150, 70);
+        slider.setMajorTickSpacing(20);
+        slider.setMinorTickSpacing(5);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.setSnapToTicks(true);
+        JPanel panel = new JPanel();
+        panel.add(slider);
+        setContentPane(panel);
+        pack();
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Win11JSlider().setVisible(true));
+    }
+}
---
+++ src/main/java/swing/Win12JTabbedPane.java
@@ -0,0 +1,24 @@
+package swing;
+
+import javax.swing.*;
+
+/**
+ * Created by WORK on 30.09.2016.
+ */
+public class Win12JTabbedPane extends JFrame {
+    Win12JTabbedPane() {
+        super("Пример использования JTabbedPane");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.WRAP_TAB_LAYOUT);
+        for (int i = 1; i <= 10; i++) {
+            JPanel panel = new JPanel();
+            panel.add(new JButton("Кнопка № " + i));
+            tabbedPane.add("Панель " + i, panel);
+        }
+        getContentPane().add(tabbedPane);
+        setSize(300,200);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Win12JTabbedPane().setVisible(true));
+    }
+}
