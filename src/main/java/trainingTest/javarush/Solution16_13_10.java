4a8aca0729f07da1d9572d99c05d1033359f2e84
jezhische <jezhische@gmail.com>
2016-11-14 15:49:40 +0200

    Some news from Alexey: java/trainingTest/javarush/Solution16_13_10FromAlexey.java

--- src/main/java/trainingTest/javarush/Solution16_13_10.java
+++ src/main/java/trainingTest/javarush/Solution16_13_10.java
@@ -23,10 +23,14 @@ import java.io.*;
 public class Solution16_13_10 {
     public static String firstFileName;
     public static String secondFileName;
-
-    public static void main(String[] args) throws InterruptedException {
+    static {
         firstFileName = "mom";
         secondFileName = "frame";
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+//        firstFileName = "mom";
+//        secondFileName = "frame";
         systemOutPrintln(firstFileName);
         systemOutPrintln(secondFileName);
     }
