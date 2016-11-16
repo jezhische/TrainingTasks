5b3234db67e2ad51251adcd2d91690d95058fe2f
jezhische <jezhische@gmail.com>
2016-11-16 12:48:49 +0200

    some probes

---
+++ src/main/java/threadClass/VolatileTest1.java
@@ -0,0 +1,28 @@
+package threadClass;
+
+/**
+ * Created by WORK on 16.11.2016.
+ */
+public class VolatileTest1 implements Runnable {
+
+     static int a = 0;
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000000; i++) {}
+        System.out.printf("Thread %d started\n", a);
+        a++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+        while (i < 10) {
+            Thread t = new Thread(new VolatileTest1());
+            t.start();
+            t.join();
+            System.out.println("current Thread name is " + Thread.currentThread().getName());
+            System.out.printf("Thread %s stopped\n", t.getName());
+            i++;
+        }
+
+    }
+}
