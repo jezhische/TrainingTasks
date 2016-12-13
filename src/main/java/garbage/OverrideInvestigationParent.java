0e152bef9289b2e2c06badf9ea98594094ce5739
jezhische <jezhische@gmail.com>
2016-12-13 12:18:03 +0200

    some probes

---
+++ src/main/java/garbage/OverrideInvestigationParent.java
@@ -0,0 +1,13 @@
+package garbage;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by WORK on 13.12.2016.
+ */
+public class OverrideInvestigationParent {
+    protected synchronized Integer fff(int a, String s) throws NoSuchElementException, InterruptedException {
+        s = "hhh";
+        return (Integer) a;
+    }
+}
