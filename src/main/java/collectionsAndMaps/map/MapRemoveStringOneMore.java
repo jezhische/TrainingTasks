637792a6bd4997ad5f376bd5940fe78ddd9f4309
jezhische <jezhische@gmail.com>
2017-02-22 19:03:47 +0200

    some probes

--- src/main/java/collectionsAndMaps/map/MapRemoveStringOneMore.java
+++ src/main/java/collectionsAndMaps/map/MapRemoveStringOneMore.java
@@ -26,34 +26,59 @@ public class MapRemoveStringOneMore {
         }
         /* простой способ порезать текст на слова: **/
         String[] text = buildText.toString().split("\\W+");
-        /* более сложный способ: **/
-        Pattern pattern = Pattern.compile("\\w+");
-        Matcher matcher = pattern.matcher(buildText.toString());
-//        String textAnotherWay[] = null;
-//        int count = matcher.groupCount();
-//        if (count != 0)
-//            textAnotherWay = new String[count];
-//        for (int i = 0; i < count; i++)
-//            textAnotherWay[i] = matcher.group();
-
-
-
-
+//        StringTokenizer st = new StringTokenizer(buildText.toString());
+//        ArrayList<String> text2 = new ArrayList<>();
+//        while(st.hasMoreElements())
+//            text2.add(st.nextToken(" "));
         NavigableMap<String, Integer> map = new TreeMap<>();
-        int i = 0;
-        while(matcher.find()) {
-            map.put(matcher.group(), i);
-            i++;
-        }
-//        String textAnotherWay[] = new String[i];
-        if (map.size() != 0) {
-            for (Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator(); iterator.hasNext();)
-                System.out.print(iterator.next().getKey() + ":" + iterator.next().getValue() + "; ");
-        }
+        for (int i = 0; i < text.length; i++)
+            map.put(text[i], i);
+        Map.Entry<String, Integer> en = map.lastEntry();
+        System.out.println(map.get("specify"));
+        System.out.println(en.getKey() + en.getValue());
+        Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+        for (Map.Entry<String, Integer> entry : entrySet)
+            System.out.print(entry.getKey() + "_" + entry.getValue() + "; ");
+
+//        for (Iterator<Map.Entry<String, Integer>> iterator = entrySet.iterator();iterator.hasNext();)
+//            iterator.next();
+//            System.out.print(m.getKey() + "_" + m.getValue() + "; ");
+        System.out.println();
+        Iterator<Map.Entry<String, Integer>> iterator = entrySet.iterator();
+//        map.remove("specify");
+        System.out.println(iterator.next());
+
+
+//        /* более сложный способ: **/
+//        Pattern pattern = Pattern.compile("\\w+");
+//        Matcher matcher = pattern.matcher(buildText.toString());
+////        String textAnotherWay[] = null;
+////        int count = matcher.groupCount();
+////        if (count != 0)
+////            textAnotherWay = new String[count];
+////        for (int i = 0; i < count; i++)
+////            textAnotherWay[i] = matcher.group();
+//
+//
+//
+//
+//        NavigableMap<String, Integer> map = new TreeMap<>();
+//        int i = 0;
+//        while(matcher.find()) {
+//            map.put(matcher.group(), i);
+//            i++;
+//        }
+////        String textAnotherWay[] = new String[i];
+//        if (map.size() != 0) {
+//            for (Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator(); iterator.hasNext();)
+//                System.out.print(iterator.next().getKey() + ":" + iterator.next().getValue() + "; ");
+//        }
 
         System.out.println(buildText.toString());
         System.out.println(Arrays.toString(text));
-        System.out.println(map.size());
+
+//        System.out.println(text2);
+//        System.out.println(map.size());
 //        System.out.println(Arrays.toString(textAnotherWay));
 //        System.out.println(count);
 
