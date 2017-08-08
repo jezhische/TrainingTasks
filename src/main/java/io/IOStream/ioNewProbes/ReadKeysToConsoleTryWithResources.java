87885dc896496ccd9a7abfc52219d3d3b4f615af
jezhische <jezhische@gmail.com>
2017-08-08 18:01:46 +0300

    iostream probes

---
+++ src/main/java/io/IOStream/ioNewProbes/ReadKeysToConsoleTryWithResources.java
@@ -0,0 +1,22 @@
+package io.IOStream.ioNewProbes;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by WORK_x64 on 07.08.2017.
+ */
+public class ReadKeysToConsoleTryWithResources {
+    public static void main(String[] args) {
+        System.out.println("~ to exit");
+        try(InputStreamReader input = new InputStreamReader(System.in)) {
+            char c;
+            do {
+                c = (char)input.read();
+                System.out.print(c);
+            } while (c != '~');
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
