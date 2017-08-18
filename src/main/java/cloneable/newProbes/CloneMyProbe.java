26d15d093741558f246e5837fe3493d6a861878b
jezhische <jezhische@gmail.com>
2017-08-18 19:26:23 +0300

    continuing probes with clone

--- src/main/java/cloneable/newProbes/CloneMyProbe.java
+++ src/main/java/cloneable/newProbes/CloneMyProbe.java
@@ -1,17 +1,19 @@
 package cloneable.newProbes;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Random;
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.*;
 
 public class CloneMyProbe implements Cloneable {
     private int initial;
     private Comparator<RandPoint> comparator = Comparator.comparing(RandPoint::getId);
     private Queue<RandPoint> prQueue;
+    private String name;
 
-    public CloneMyProbe(int initial) {
+    public CloneMyProbe(int initial, String name) {
         this.initial = initial;
+        this.name = name;
         prQueue = populateAndGetQueue();
     }
     public Queue<RandPoint> populateAndGetQueue() {
@@ -22,9 +24,30 @@ public class CloneMyProbe implements Cloneable {
         return orderedQueue;
     }
 
+//    @Override
+//    protected Object clone() throws CloneNotSupportedException {
+//        return super.clone();
+//    }
+
+        @Override
+    protected CloneMyProbe clone() throws CloneNotSupportedException {
+        CloneMyProbe other = (CloneMyProbe) super.clone();
+        other.setComparator(this.getComparator());
+        Queue<RandPoint> otherQueue = new PriorityQueue<>(initial, comparator);
+        Iterator<RandPoint> iterator = prQueue.iterator();
+        while(iterator.hasNext()) {
+            otherQueue.add(iterator.next());
+        }
+//        other.setPrQueue(prQueue);
+        other.setPrQueue(otherQueue);
+        other.setName(this.getName());
+        return other;
+    }
+
     @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
+    public String toString() {
+        return this.getName() + ": initial - " + this.getInitial() + ", comparator - " + this.getComparator() +
+                ", prQueue - " + this.getPrQueue();
     }
 
     public int getInitial() {
@@ -50,6 +73,14 @@ public class CloneMyProbe implements Cloneable {
     public void setPrQueue(Queue<RandPoint> prQueue) {
         this.prQueue = prQueue;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 class RandPoint {
     private int id;
@@ -72,11 +103,19 @@ class RandPoint {
 
 
 class DoIt {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         int initial = 5;
-        CloneMyProbe original = new CloneMyProbe(initial);
-        Queue<RandPoint> naturalOrderQueue = original.populateAndGetQueue();
-        naturalOrderQueue.forEach(rp -> System.out.println(rp.getId()));
+        CloneMyProbe original = new CloneMyProbe(initial, "Original");
+//        Queue<RandPoint> naturalOrderQueue = original.populateAndGetQueue();
+//        naturalOrderQueue.forEach(rp -> System.out.println(rp.getId()));
+        CloneMyProbe clone = (CloneMyProbe) original.clone();
+        System.out.println(original.toString());
+        System.out.println(clone.toString());
+        System.out.println();
+        clone.getPrQueue().add(RandPoint.getRP(1));
+        clone.setName("Clone");
+        System.out.println(original.toString());
+        System.out.println(clone.toString());
 
 
     }
